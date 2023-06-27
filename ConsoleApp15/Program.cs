4c7ccc30b573ccd31adebacec06cb5da7577621c
1dNDN <42769281+1dNDN@users.noreply.github.com>
2023-06-27 19:45:15 +0300

    Initial commit

---
+++ ConsoleApp15/Program.cs
@@ -0,0 +1,6 @@
+﻿using System;
+using System.Net.Http;
+
+var client = new HttpClient();
+var text = client.GetStringAsync("https://google.com").Result;
+Console.WriteLine($"Got {text.Length} characters");
